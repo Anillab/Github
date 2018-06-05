5206be816f0b5e87556f17dc8e433e91da847e7b
Anillab <anillabusolo@gmail.com>
2018-06-06 10:03:37 +1245

    modified 3 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RoutingModule,Routes } from "@angular/router";
-import {SearchComponent} from "../search/search.component";
+import { RouterModule, Routes} from '@angular/router';
+import {HttpClient} from '@angular/common/http'
+import { SearchComponent } from '../search/search.component';
+import { RoutingModule } from './routing/routing.module';
+//import { NotFoundComponent } from '../not-found/not-found.component';
+
 
 const routes:Routes=[
-  {path:"users",component:SearchComponent}
+  {path:"users",component:SearchComponent},
+  {path:"",redirectTo:"/users",pathMatch:"full"},
+  //{path:'**',component:NotFoundComponent}
 ]
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
+  exports:[RouterModule],
   declarations: []
 })
-export class RoutesModule { }
+export class RoutingModule { }
